99cb10ce3cc211c1492878da0d84e62a61bff816
Brandon Nichols <brandonaronnichols@gamil.com>
2019-08-22 22:26:44 -0500

    :sparkles: Creating Basic Workout Type Cards
    added workout type cards ins Journal.js, currently they are part of Journal.js instead of a seperate component will probably be changed later, currently getting dummy information as array of strings passed from App.js

--- buff/src/App.js
+++ buff/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Journal from './Journal';
 
 function App() {
+
+    const [workouts, setWorkouts] = useState(['Leg Day', 'Push Day', 'Arms Day']);
+
   return (
     <div className="App">
+      
+      <Route path='/journal' 
+             render={(props) => (
+             <Journal {...props} workouts={workouts} />
+             )} />
     </div>
   );
 }
--- buff/src/Journal.js
+++ buff/src/Journal.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components';
 
-export default function Journal() {
+export default function Journal({workouts}) {
+    
+
+    const Workout = styled.div`
+        max-width: 25%;
+        height: 100px;
+        background: red;
+    `;
+
+    // const createWorkoutType = () => {
+    //     return (<Workout>
+    //                 <h1>New Workout</h1>
+    //             </Workout>);
+    // };
+
     return (
         <div>
-            
+            {workouts.map(workout => {
+                return(
+                <Workout>
+                    <h1>{workout}</h1>
+                </Workout>
+                )
+            })}
         </div>
     )
 }
