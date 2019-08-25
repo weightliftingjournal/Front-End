e401c78a4426e59e4c9ee8c8f984f475c8687af6
JamesB <jblazer1@yahoo.com>
2019-08-25 13:59:37 -0700

    Login Form

--- buff/src/App.js
+++ buff/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Login from "./Login.js";
 
 function App() {
   return (
     <div className="App">
+      <Login />
     </div>
   );
 }
--- buff/src/Login.js
+++ buff/src/Login.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Form, Field, withFormik } from "formik";
+import { Link } from "react-router-dom";
 
-export default function Login() {
-    return (
-        <div>
-            
+const Login = () => {
+  return (
+    <div className="background">
+      <div className="login-form">
+        <h2>Please Login Below</h2>
+        <div className="form-container">
+          <Form className="some-form">
+            <h4>Username</h4>
+            <Field
+              component="input"
+              type="email"
+              name="username"
+              placeholder="enter username"
+            />
+            <h4>Password</h4>
+            <Field
+              component="input"
+              type="password"
+              name="password"
+              placeholder="enter password"
+            />
+
+            <button type="submit">Submit</button>
+          </Form>
         </div>
-    )
-}
+        <h2>
+          No Accout? <span>Sign up!</span>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+const LoginForm = withFormik({
+  mapPropsToValues(values) {
+    return {
+      username: values.username || "",
+      password: values.password || ""
+    };
+  }
+})(Login);
+
+export default LoginForm;
