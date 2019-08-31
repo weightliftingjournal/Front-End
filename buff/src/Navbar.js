d7b8040ca279518b01071b5e9699c4463cf6d249
Brandon Nichols <brandonaronnichols@gamil.com>
2019-08-30 20:59:37 -0500

    :sparkles: Logout
    added logout feature to app

--- buff/src/Navbar.js
+++ buff/src/Navbar.js
@@ -3,6 +3,13 @@ import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
 export default function Navbar() {
+
+    const logout = () => {
+        localStorage.removeItem('id');
+        localStorage.removeItem('token');
+        //props.history.push('/');
+    }
+
     return (
         <div className="navbar">
            <div className="nav-left">
@@ -11,6 +18,7 @@ export default function Navbar() {
            <div className="nav-right">
            <a href="https://buff-marketing-webpt9-john.netlify.com/">Home</a>
            <NavLink to='/'>Login</NavLink>
+           <NavLink to='/' onClick={logout}>Logout</NavLink>
            {/* <a href="#Login">Login</a> */}
            </div>
        </div>
